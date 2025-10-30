4e5963bd64097f10e290091e71ee41616cb8062a
hichamSalhi <hicham.salhi@usmba.ac.ma>
2025-10-30 21:05:47 +0100

    I just learned how to do a chat application in my local system using threads.

--- Connection/SimpleChatClientA.java
+++ Connection/SimpleChatClientA.java
@@ -1,31 +1,44 @@
 package Connection;
 
 import java.awt.BorderLayout;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.nio.channels.Channels;
 import java.nio.channels.SocketChannel;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.WindowConstants;
 
 public class SimpleChatClientA {
+    private JTextArea incoming;
+    private BufferedReader reader;
     private JTextField outgoing;
     private PrintWriter writer;
 
     public void go() {
         setUpNetworking();
-
+        JScrollPane scroller = createScrollableTextArea();
         outgoing = new JTextField(20);
         JButton sendButton = new JButton("Send");
         sendButton.addActionListener(_ -> sendMessage());
         JPanel mainPanel = new JPanel();
+        mainPanel.add(scroller);
         mainPanel.add(outgoing);
         mainPanel.add(sendButton);
+
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.execute(new IncommingReader());
+
         JFrame frame = new JFrame("Ludicrously Simple Chat Client");
         frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
         frame.setSize(400, 100);
@@ -37,6 +50,9 @@ public class SimpleChatClientA {
         try {
             InetSocketAddress serverAddress = new InetSocketAddress("127.0.0.1", 5000);
             SocketChannel socketChannel = SocketChannel.open(serverAddress);
+            // The reader will read messaages coming from the server
+            reader = new BufferedReader(Channels.newReader(socketChannel, "utf-8"));
+            // The writer will send messages to the server
             writer = new PrintWriter(Channels.newWriter(socketChannel, "utf-8"));
         } catch (IOException e) {
             e.getStackTrace();
@@ -48,6 +64,32 @@ public class SimpleChatClientA {
         writer.flush();
         outgoing.setText("");
         outgoing.requestFocus();
+    }
+
+    private JScrollPane createScrollableTextArea() {
+        incoming = new JTextArea(15, 30);
+        incoming.setLineWrap(true);
+        incoming.setWrapStyleWord(true);
+        incoming.setEditable(false);
+        JScrollPane scroller = new JScrollPane(incoming);
+        scroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        scroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        return scroller;
+    }
+
+    public class IncommingReader implements Runnable {
+        @Override
+        public void run() {
+            String message;
+            try {
+                while ((message = reader.readLine()) != null) {
+                    System.out.println("read " + message);
+                    incoming.append(message + "\n");
+                }
+            } catch (IOException e) {
+                e.getStackTrace();
+            }
+        }
     }
 
     public static void main(String[] args) {
